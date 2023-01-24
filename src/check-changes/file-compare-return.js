c290b63c974f23a48417e09b6ef45918f5265b96
vagnersiqueirajr <vagner.junior@shipeezi.com>
2023-01-24 14:22:02 -0300

    fix: fixed object filesFailedOrSuccess

--- src/check-changes/file-compare-return.js
+++ src/check-changes/file-compare-return.js
@@ -5,11 +5,7 @@ function fileCompareReturn(oldStructure, newStructure) {
   const countServices = oldStructure[1] - newStructure[1];
   const countEntities = oldStructure[2] - newStructure[2];
 
-  let filesFailedOrSuccess = {
-    controller,
-    service,
-    entity,
-  }
+  let filesFailedOrSuccess = {};
   
   countControllers > 0 ? filesFailedOrSuccess.controller = Status.FAILED : filesFailedOrSuccess.controller = Status.SUCCESS
   countServices > 0 ? filesFailedOrSuccess.service = Status.FAILED : filesFailedOrSuccess.service = Status.SUCCESS
